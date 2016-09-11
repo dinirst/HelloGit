589306a6c51ba9a0c2e4751dc2bacea7a3ef6ddf
diniristanti <dinieka07@outlook.com>
2016-09-11 08:26:32 +0700

    Buat tampilan dan sebagainya Validasi dan memfungsikan button Validasi nama dan spinner kelas

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl6010/tugas1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl6010/tugas1/MainActivity.java
@@ -7,27 +7,34 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tvCM, tvK, tvJK, tvMT, tvHasil;
+    TextView tvCM, tvK, tvJK, tvMT, tvHN, tvHC, tvGD, tvMR;
     CheckBox cbLT, cbSP, cbWR;
     RadioButton rbM, rbFM;
     EditText etNama;
     Spinner spKL;
     Button btSB;
+    RadioGroup rgG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        rgG = (RadioGroup) findViewById(R.id.radioGroupG);
+
         tvCM = (TextView) findViewById(R.id.textViewCM);
         tvK = (TextView) findViewById(R.id.textViewK);
         tvJK = (TextView) findViewById(R.id.textViewJK);
         tvMT = (TextView) findViewById(R.id.textViewMT);
-        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        tvHN = (TextView) findViewById(R.id.textViewHN);
+        tvGD = (TextView) findViewById(R.id.textViewGD);
+        tvHC = (TextView) findViewById(R.id.textViewHC);
+        tvMR = (TextView) findViewById(R.id.textViewMR);
 
         cbLT = (CheckBox) findViewById(R.id.checkBoxLT);
         cbSP = (CheckBox) findViewById(R.id.checkBoxSP);
@@ -45,10 +52,37 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                String name = etNama.getText().toString();
-                tvHasil.setText("Name : " + name + "\n Class :" + spKL.getSelectedItem().toString());
+                doProcess();
+                doClick();
             }
         });
 
+    }
+
+    private void doProcess() {
+        String name = etNama.getText().toString();
+        tvHN.setText("Name : " + name);
+    }
+
+    private void doClick() {
+        tvHC.setText("Class : " + spKL.getSelectedItem().toString());
+
+    }
+
+    private boolean isValid() {
+        boolean valid = true;
+
+        String name = etNama.getText().toString();
+
+        if (name.isEmpty()) {
+            etNama.setError("You should fill your full name");
+            valid = false;
+        } else if (etNama.length() < 3) {
+            etNama.setError("Fill your full name");
+            valid = false;
+        } else {
+            etNama.setError(null);
+        }
+        return false;
     }
 }
