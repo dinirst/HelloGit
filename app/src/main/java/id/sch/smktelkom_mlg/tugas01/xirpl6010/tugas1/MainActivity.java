b5b5319dfe19e87b49c1566688ae48705d04eb3f
diniristanti <dinieka07@outlook.com>
2016-09-11 07:08:43 +0700

    Buat tampilan dan sebagainya Validasi dan memfungsikan button

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl6010/tugas1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl6010/tugas1/MainActivity.java
@@ -2,15 +2,20 @@ package id.sch.smktelkom_mlg.tugas01.xirpl6010.tugas1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView tvCM, tvK, tvJK, tvMT, tvHasil;
     CheckBox cbLT, cbSP, cbWR;
     RadioButton rbM, rbFM;
+    EditText etNama;
+    Spinner spKL;
     Button btSB;
 
     @Override
@@ -18,6 +23,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tvCM = (TextView) findViewById(R.id.textViewCM);
+        tvK = (TextView) findViewById(R.id.textViewK);
+        tvJK = (TextView) findViewById(R.id.textViewJK);
+        tvMT = (TextView) findViewById(R.id.textViewMT);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
+        cbLT = (CheckBox) findViewById(R.id.checkBoxLT);
+        cbSP = (CheckBox) findViewById(R.id.checkBoxSP);
+        cbWR = (CheckBox) findViewById(R.id.checkBoxWR);
+
+        rbM = (RadioButton) findViewById(R.id.radioButtonM);
+        rbFM = (RadioButton) findViewById(R.id.radioButtonFM);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+
+        spKL = (Spinner) findViewById(R.id.spinnerKL);
+
+        btSB = (Button) findViewById(R.id.buttonSB);
+        btSB.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String name = etNama.getText().toString();
+                tvHasil.setText("Name : " + name + "\n Class :" + spKL.getSelectedItem().toString());
+            }
+        });
 
     }
 }
